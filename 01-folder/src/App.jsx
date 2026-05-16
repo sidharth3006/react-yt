d24742bf93eb653ed92f304c6b4a30da3736e410
Sidharth <sidharth2020sharma@gmail.com>
2026-05-16 23:06:19 +0530

    First commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+      <div>
+        <h1>App</h1>
+      </div> 
+      <div>
+        <h2>App</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
